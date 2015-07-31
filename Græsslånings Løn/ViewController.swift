b3c65f9a1cdd4f2197e1da8da0538534675bab8a
Martin Lok <Martinlok@icloud.com>
2015-08-01 00:24:11 +0200

    Initial Commit

---
+++ Græsslånings Løn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Græsslånings Løn
+//
+//  Created by Martin Lok on 01/08/2015.
+//  Copyright © 2015 Martin Lok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
